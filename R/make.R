9a69517e9d9ea301341104ad3987828f79bcdd92
Jeroen Ooms <jeroenooms@gmail.com>
2020-07-25 14:47:47 +0200

    More testing on Windows

--- R/make.R
+++ R/make.R
@@ -14,6 +14,7 @@
 #' @param makefile path to the `Makefile`. Defaults to the `Makeconf` which
 #' R uses when building R packages.
 #' @examples # Test the CXX compiler
+#' if(cxx_info()$available){
 #' testprog <- '#include <iostream>\nint main() {std::cout << "Hello World!";}'
 #' writeLines(testprog, con = 'testprog.cc')
 #' make('testprog')
@@ -21,6 +22,7 @@
 #' # Test and cleanup
 #' system('./testprog')
 #' unlink('testprog*', recursive = TRUE)
+#' }
 #'
 make <- function(target = 'all', makefile = r_makeconf_path()){
   status <- sys::exec_wait(r_make_path(), c(target, '-f', makefile))
