3397bb01f754e235e5de4b00edd29986678f8107
Jeroen Ooms <jeroenooms@gmail.com>
2020-09-08 16:27:34 +0200

    Workaround for CRAN

--- tests/testthat/test-sysdeps.R
+++ tests/testthat/test-sysdeps.R
@@ -2,7 +2,9 @@ test_that("sysdeps works", {
   skip_on_os('windows')
   expect_true(any(grepl('libcurl', package_links_to('curl'))))
   sysdeps <- package_sysdeps('curl')
-  expect_true(any(grepl('libcurl', sysdeps$shlib)))
+  if(running_on('linux') || getRversion() >= 4){
+    expect_true(any(grepl('libcurl', sysdeps$shlib)))
+  }
   if(running_on('linux')){
     expect_true(any(grepl('curl', sysdeps$package)))
   }
