9701a0b4b828492ec95c13882417bbd7d21e9dff
Jeroen Ooms <jeroenooms@gmail.com>
2020-09-12 16:28:57 +0200

    Fix CRAN error

--- tests/testthat/test-sysdeps.R
+++ tests/testthat/test-sysdeps.R
@@ -1,8 +1,8 @@
 test_that("sysdeps works", {
   skip_on_os('windows')
-  expect_true(any(grepl('libcurl', package_links_to('curl'))))
   sysdeps <- package_sysdeps('curl')
   if(running_on('linux') || getRversion() >= 4){
+    expect_true(any(grepl('libcurl', package_links_to('curl'))))
     expect_true(any(grepl('libcurl', sysdeps$shlib)))
   }
   if(running_on('linux')){
