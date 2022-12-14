8aaddc2160f953f042baa80b2f94dc5e2f3cf322
Jeroen Ooms <jeroenooms@gmail.com>
2022-12-14 15:50:23 +0100

    Detect rust packages (#2)

--- R/sysdeps.R
+++ R/sysdeps.R
@@ -49,6 +49,14 @@ package_sysdeps <- function(pkg, lib.loc = NULL){
 
 #' @export
 #' @rdname sysdeps
+dll_has_symbol <- function(dll, symbol){
+  cmd <- sprintf('nm -gp %s | grep %s', dll, symbol)
+  out <- system(cmd, intern = TRUE)
+  isTRUE(length(out) && grepl(symbol, out))
+}
+
+#' @export
+#' @rdname sysdeps
 package_sysdeps_string <- function(pkg, lib.loc = NULL){
   df <- package_sysdeps(pkg = pkg, lib.loc = lib.loc)
   df <- subset(df, !is.na(df$package))
@@ -64,6 +72,14 @@ package_links_to <- function(pkg, lib.loc = NULL){
   dll <- file.path(pkgpath, sprintf('libs%s/%s%s', Sys.getenv('R_ARCH'), pkg, .Platform$dynlib.ext))
   if(!file.exists(dll)) # No compiled code
     return(character())
+
+  # On Windows, rustc is not part of the toolchain
+  if(!running_on('windows') && dll_has_symbol(dll, 'rust_begin_unwind')){
+    rustc <- Sys.which('rustc')
+    if(nchar(rustc)){
+      return(rustc)
+    }
+  }
   structure(if(running_on('macos')){
     links_to_macos(dll)
   } else if(running_on('windows')) {
