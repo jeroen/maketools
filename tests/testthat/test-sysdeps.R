b3c1d4863ae8f72677592305815bc21fbbdcd54d
Jeroen Ooms <jeroenooms@gmail.com>
2023-06-29 09:51:35 +0200

    Fix getRversion type (requested by CRAN)

--- tests/testthat/test-sysdeps.R
+++ tests/testthat/test-sysdeps.R
@@ -1,7 +1,7 @@
 test_that("sysdeps works", {
   skip_on_os('windows')
   sysdeps <- package_sysdeps('curl')
-  if(running_on('linux') || getRversion() >= 4){
+  if(running_on('linux') || getRversion() >= "4"){
     expect_true(any(grepl('libcurl', package_links_to('curl'))))
     expect_true(any(grepl('libcurl', sysdeps$shlib)))
   }
