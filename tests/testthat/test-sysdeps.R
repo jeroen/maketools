9c2e7a3098b3de5cf7970f515d046a5bf4112993
Jeroen Ooms <jeroenooms@gmail.com>
2025-01-25 13:22:35 +0100

    Skip more unit tests for statically linked libcurl

--- tests/testthat/test-sysdeps.R
+++ tests/testthat/test-sysdeps.R
@@ -1,12 +1,9 @@
 test_that("sysdeps works", {
   skip_on_os('windows')
+  skip_on_os('mac')
+  skip_on_cran()
   sysdeps <- package_sysdeps('curl')
-  if(running_on('linux') || getRversion() >= "4"){
-    expect_true(any(grepl('libcurl', package_links_to('curl'))))
-    expect_true(any(grepl('libcurl', sysdeps$shlib)))
-  }
-  if(running_on('linux')){
-    skip_on_cran() #BDR may build libcurl from source
-    expect_true(any(grepl('curl', sysdeps$package)))
-  }
+  expect_true(any(grepl('libcurl', package_links_to('curl'))))
+  expect_true(any(grepl('libcurl', sysdeps$shlib)))
+  expect_true(any(grepl('curl', sysdeps$package)))
 })
